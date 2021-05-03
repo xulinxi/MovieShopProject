7bb3300ca1fc3a001fc5ca1ae05c66ede4463601
xulinxi <52603892+xulinxi@users.noreply.github.com>
2021-05-03 09:01:50 -0400

    Updated json file and created MovieGenre entities
    TODO: Continue creating all table entities

--- Infrastructure/Data/MovieShopDbContext.cs
+++ Infrastructure/Data/MovieShopDbContext.cs
@@ -16,5 +16,20 @@ namespace Infrastructure.Data
         }
 
         public DbSet<Genre> Genres { get; set; }
+        public DbSet<MovieGenre> MovieGenre { get; set; }
+        public DbSet<MovieCast> MovieCast { get; set; }
+        public DbSet<Cast> Cast { get; set; }
+        public DbSet<Crew> Crew { get; set; }
+        public DbSet<MovieCrew> MovieCrew { get; set; }
+        public DbSet<Movie> Movie { get; set; }
+        public DbSet<Review> Review { get; set; }
+        public DbSet<User> User { get; set; }
+        public DbSet<Favorite> Favorite { get; set; }
+        public DbSet<Role> Role { get; set; }
+        public DbSet<UserRole> UserRole { get; set; }
+        public DbSet<Purchase> Purchase { get; set; }
+        public DbSet<Trailer> Trailer { get; set; }
+
+
     }
 }
