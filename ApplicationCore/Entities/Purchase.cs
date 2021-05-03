0ccf99537676ac4dc6b4313663d0b40fd304d749
xulinxi <52603892+xulinxi@users.noreply.github.com>
2021-05-03 09:34:30 -0400

    Created entity classes for all tables

---
+++ ApplicationCore/Entities/Purchase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Entities
+{
+    class Purchase
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+
+        // Note PurchaseNumber should be unique
+        public int PurchaseNumber { get; set; }
+        public decimal TotalPrice { get; set; }
+        public DateTime PurchaseDateTime { get; set; }
+        public int MovieId { get; set; }
+    }
+}
---
+++ ApplicationCore/Entities/Trailer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Entities
+{
+    class Trailer
+    {
+        public int Id { get; set; }
+        public int MovieID { get; set; }
+        public string TrailerUrl { get; set; }
+        public string Name { get; set; }
+    }
+}
