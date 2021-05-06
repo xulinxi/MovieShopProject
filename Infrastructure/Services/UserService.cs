649759e573bf6c2ade5014f8ddd721e501da442f
xulinxi <52603892+xulinxi@users.noreply.github.com>
2021-05-06 11:14:21 -0400

    5/6 lecture updates

---
+++ Infrastructure/Services/UserService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ApplicationCore.Models.Request;
+using ApplicationCore.Models.Response;
+using ApplicationCore.ServiceInterfaces;
+
+namespace Infrastructure.Services
+{
+    public class UserService: IUserService
+    {
+
+    }
+}
