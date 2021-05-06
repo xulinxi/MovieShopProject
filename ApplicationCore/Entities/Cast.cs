5f09528c8b6c4bc3d5af01ae71fa0dc81fe1578a
xulinxi <52603892+xulinxi@users.noreply.github.com>
2021-05-06 10:25:06 -0400

    Update Cast.cs

--- ApplicationCore/Entities/Cast.cs
+++ ApplicationCore/Entities/Cast.cs
@@ -13,5 +13,6 @@ namespace ApplicationCore.Entities
         public string Gender { get; set; }
         public string TmdbUrl { get; set; }
         public string ProfilePath { get; set; }
+        public ICollection<MovieCast> MovieCasts { get; set; }
     }
 }
