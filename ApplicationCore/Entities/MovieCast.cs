a795695156fdf49940eda884247b788e979f7fdf
xulinxi <52603892+xulinxi@users.noreply.github.com>
2021-05-06 10:25:53 -0400

    Update MovieCast.cs

--- ApplicationCore/Entities/MovieCast.cs
+++ ApplicationCore/Entities/MovieCast.cs
@@ -8,8 +8,17 @@ namespace ApplicationCore.Entities
 {
     public class MovieCast
     {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public Guid PurchaseNumber{get; set; }
+        public decimal TotalPrice { get; set; }
+        public DateTime PurchaseDateTime { get; set; }
         public int MovieId { get; set; }
-        public int CastId { get; set; }
-        public string Character { get; set; }
+        //public int CastId { get; set; }
+        //public string Character { get; set; }
+        public Movie Movie { get; set; }
+        public User Customer { get; set; }
+        public object CastId { get; set; }
+        //public Cast Cast { get; set; }
     }
 }
