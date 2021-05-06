78db10967fb8138bbfb2bb706ea6d30dc87e073a
xulinxi <52603892+xulinxi@users.noreply.github.com>
2021-05-06 10:25:42 -0400

    Update Favorite.cs

--- ApplicationCore/Entities/Favorite.cs
+++ ApplicationCore/Entities/Favorite.cs
@@ -8,8 +8,10 @@ namespace ApplicationCore.Entities
 {
     public class Favorite
     {
-        public int Id { get; set; }
+        //public int Id { get; set; }
         public int MovieId { get; set; }
         public int UserId { get; set; }
+        public User User { get; set; }
+        public Movie Movie { get; set; }
     }
 }
