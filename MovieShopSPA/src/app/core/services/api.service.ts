9a542b679ad7269bb193cd55fb912f3bd83da876
xulinxi <52603892+xulinxi@users.noreply.github.com>
2021-05-12 11:45:53 -0400

    api

--- MovieShopSPA/src/app/core/services/api.service.ts
+++ MovieShopSPA/src/app/core/services/api.service.ts
@@ -1,10 +1,38 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  // HttpClient => 
-  constructor() { }
+  // HttpClient => used to communicate with API
+  constructor(protected http: HttpClient) { }
+
+
+  // getting array of json objects
+  getList() {
+
+  }
+
+  // get single json object movie/id
+  getOne() {
+
+  }
+
+  // post something
+  create() {
+
+  }
+
+  // PUT
+  update() {
+
+  }
+
+  // DELETE
+  delete() {
+
+  }
+
 }
