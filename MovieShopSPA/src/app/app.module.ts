221a873d135e1be104fefe60a572c6a817c715de
xulinxi <52603892+xulinxi@users.noreply.github.com>
2021-05-12 10:35:27 -0400

    angular app

--- MovieShopSPA/src/app/app.module.ts
+++ MovieShopSPA/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GenresComponent } from './genres/genres.component';
+import { HeaderComponent } from './core/layout/header/header.component';
+import { FooterComponent } from './core/layout/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GenresComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
