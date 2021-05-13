00f43ad4e7d23b0b233207333b35d538bcce7311
xulinxi <52603892+xulinxi@users.noreply.github.com>
2021-05-13 12:00:48 -0400

    components initiazation

--- MovieShopSPA/src/app/app-routing.module.ts
+++ MovieShopSPA/src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateCastComponent } from './admin/create-cast/create-cast.component';
+import { CreateMovieComponent } from './admin/create-movie/create-movie.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { HomeComponent } from './home/home.component';
+import { MovieCardListComponent } from './movies/movie-card-list/movie-card-list.component';
 
-const routes: Routes = [];
+// specify all the routes belonging to Angular appllication
+const routes: Routes = 
+[
+  // when path is empty -> Home page/component
+{ path: "", component: HomeComponent }, // reroutes to component view when the link is clicked
+{ path: "login", component: LoginComponent },
+{ path: "register", component: RegisterComponent },
+{ path: "genres/:id", component: MovieCardListComponent },
+{ path: "admin/createmovie", component: CreateMovieComponent },
+{ path: "admin/createcase", component: CreateCastComponent }
+
+];
+
+// // specify all the routes belonging to Angular application
+// const routes: Routes =
+//   [
+//     { path: "", component: HomeComponent },
+//     { path: "login", component: LoginComponent },
+//     { path: "register", component: RegisterComponent },
+//     { path: "admin/createmovie", component: CreateMovieComponent },
+//     { path: "admin/createcasr", component: CreateCastComponent }
+//   ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- MovieShopSPA/src/app/app.module.ts
+++ MovieShopSPA/src/app/app.module.ts
@@ -8,6 +8,17 @@ import { GenresComponent } from './genres/genres.component';
 import { HeaderComponent } from './core/layout/header/header.component';
 import { FooterComponent } from './core/layout/footer/footer.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { HomeComponent } from './home/home.component';
+import { CreateMovieComponent } from './admin/create-movie/create-movie.component';
+import { CreateCastComponent } from './admin/create-cast/create-cast.component';
+import { MovieCardListComponent } from './movies/movie-card-list/movie-card-list.component';
+import { MovieCardComponent } from './shared/components/movie-card/movie-card.component';
+import { FavoriteButtonComponent } from './shared/components/favorite-button/favorite-button.component';
+import { NotFoundComponent } from './shared/components/not-found/not-found.component';
+import { PurchasesComponent } from './user/purchases/purchases.component';
+import { FavoritesComponent } from './user/favorites/favorites.component';
 // import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
@@ -15,7 +26,18 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     AppComponent,
     GenresComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    CreateMovieComponent,
+    CreateCastComponent,
+    MovieCardListComponent,
+    MovieCardComponent,
+    FavoriteButtonComponent,
+    NotFoundComponent,
+    PurchasesComponent,
+    FavoritesComponent
   ],
   imports: [
     BrowserModule,
